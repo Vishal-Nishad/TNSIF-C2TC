98b9bc3589c8075ebaad9915c64dc51cae82b7c2
Vishal-Nishad <vishal.n1020@gmail.com>
2024-02-17 13:50:01 +0530

    first cmmit

---
+++ src/com/daythree/practice/Constructor.java
@@ -0,0 +1,17 @@
+package com.daythree.practice;
+
+public class Constructor {
+	
+	public static void main(String args[]) {
+		Studen s1=new Studen();
+		
+	}
+
+}
+ class Studen{
+	 String name;
+	 int rollnum;
+	 Studen(){
+		 System.out.println("My name is : ");
+	 }
+ }
